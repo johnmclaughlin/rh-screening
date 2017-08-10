6a1182ebb692096dbefe690d78712e3bf3171def
john mclaughlin <john.n.mclaughlin@gmail.com>
2017-08-10 15:45:27 -0400

    test-CSS inital commit

--- redhat.js
+++ redhat.js
@@ -2,11 +2,13 @@ function   remoteMathService (cb) {
     var  one, two; 
 
     callOneService( function (err, num) {
-        one  =  1; 
+        one  =  num; 
+        console.log('one: '+one);
     });
 
     callTwoService( function (err, num) { 
-        two  =  2;
+        two  =  num;
+        console.log('two: '+two);
     });
 
     return  cb( undefined , one  +  two); 
@@ -16,18 +18,20 @@ function   callOneService (cb) {
     setTimeout ( function () {
         return  cb( undefined ,  1 ); 
     },  1000 );
+    console.log('cb: '+cb);
 }
 
 function   callTwoService (cb) {  
     setTimeout ( function () {
         return  cb( undefined ,  2 ); 
     },  1500 );
+    console.log('cb: '+cb);
 }
 
 remoteMathService( function (err, answer) {  
     if  (err)  console .log ( "error " , err);  
     if  (answer  !==   3 ) {
-        console.log(answer);
+        console.log('answer: '+answer);
         console .log ( "wrong answer" , answer); 
     }  else  {
         console .log ( "correct" ); 
