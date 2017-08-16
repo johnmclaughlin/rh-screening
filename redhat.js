dd34cd5c7e25aac44e98e36907c069e632c999f5
john mclaughlin <john.n.mclaughlin@gmail.com>
2017-08-16 13:14:54 -0400

    Add timeout to correct output error

--- redhat.js
+++ redhat.js
@@ -3,35 +3,32 @@ function   remoteMathService (cb) {
 
     callOneService( function (err, num) {
         one  =  num; 
-        console.log('one: '+one);
     });
 
     callTwoService( function (err, num) { 
         two  =  num;
-        console.log('two: '+two);
     });
 
+    setTimeout ( function () {
     return  cb( undefined , one  +  two); 
+    },  2000 );
 }
 
 function   callOneService (cb) {  
     setTimeout ( function () {
         return  cb( undefined ,  1 ); 
     },  1000 );
-    console.log('cb: '+cb);
 }
 
 function   callTwoService (cb) {  
     setTimeout ( function () {
         return  cb( undefined ,  2 ); 
     },  1500 );
-    console.log('cb: '+cb);
 }
 
 remoteMathService( function (err, answer) {  
-    if  (err)  console .log ( "error " , err);  
+    if  (err)  console .log ( "error " , err);
     if  (answer  !==   3 ) {
-        console.log('answer: '+answer);
         console .log ( "wrong answer" , answer); 
     }  else  {
         console .log ( "correct" ); 
